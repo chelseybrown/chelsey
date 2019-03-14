38a21570c9a7a57473465ae84cdda9a3f4d4d982
chelseybrown <chelseybrown2009@icloud.com>
2019-03-15 09:28:25 +1300

    Add files via upload

---
+++ cc2/ps1/README.md
@@ -0,0 +1,6 @@
+## PS1 MDDN 242 2019
+
+### maeda clock
+
+I chose the 8th clock on the page, purely because it seemed the easiest and I didn't want to confuse myself as I am getting back into the swing of things.
+
---
+++ cc2/ps1/maeda_clock.js
@@ -0,0 +1,33 @@
+const spacing = 50;
+
+// Update this function to draw you own maeda clock on a 960x500 canvas
+function draw_clock(obj) {
+  background(0);
+ // nostroke();
+  fill(150);
+
+
+  rect(50, 150, 20, 20);
+  rect(70, 130, 60, 20);
+  rect(130, 150, 20, 40);
+  rect(110, 190, 20, 20);
+  rect(90, 210, 20, 20);
+  rect(70, 230, 20, 20); 
+  rect(50, 250, 100, 20);
+
+
+  rect(350, 150, 20, 20);
+  rect(370, 130, 20, 140);
+
+  rect(410, 130, 80, 20);
+  rect(410, 150, 20, 40);
+  rect(410, 170, 80, 20);
+  rect(470, 190, 20, 60);
+  rect(410, 230, 20, 20);
+  rect(430, 250, 40, 20);
+
+
+
+
+
+}
